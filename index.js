e87e8401e4023aa810a22344918a166fb542e163
Carine-69 <155974482+Carine-69@users.noreply.github.com>
2024-02-27 20:55:58 +0200

    whole app

---
+++ index.js
@@ -0,0 +1,51 @@
+function updatedWEather(response) {
+    let temperatureElement = document.querySelector(".temp-standard"); // Change to querySelector
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#current-data");
+    let descriptionElement  = document.querySelectorAll("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windspeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    if(time ,10) {
+       minutes = `0 ${minutes}`;
+    }
+    
+    cityElement.innerHTML = response.data.city;
+    descriptionElement[0].innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatData(date);
+    temperatureElement.innerHTML = Math.round(temperature) + "°C";
+}
+
+
+function formatData(date){
+    let minutes = date.getMinutes();
+      let hours = date.getHours();
+    let days = ["Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+];
+        let day = days[date.getDay()];
+  return `${day}  ${hours}:${minutes}`
+}
+function searchCity(city) {
+    let apiKey = "78o1309884534590dt047b4a23f7d6b9"; 
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(updatedWEather);
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#city");
+    searchCity(city.value); 
+}
+
+let searchFormElement = document.querySelector(".searchForm");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
